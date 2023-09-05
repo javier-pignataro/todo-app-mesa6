958ebe3b4471df3613e5fe1207b9466ae69f56a4
JhonatanRios <jhoto1234@gmail.com>
2023-09-05 14:21:08 -0500

    Organizar html, primeros ajustes utils y signup

--- scripts/signup.js
+++ scripts/signup.js
@@ -2,29 +2,47 @@
 /* FUNCIÓN 1: Escuchamos el submit y preparamos el envío */
 /* ---------------------------------------------- */
 form.addEventListener('submit', (e) => {
-    e.preventDefault();
+  e.preventDefault();
+  const pass = password.value;
+  const passRepetida = passwordReply.value;
+
+  if (compararContrasenias(pass, passRepetida)) {
     payload = {
-        firstName: firstName.value,
-        lastName: lastName.value,
-        email: email.value,
-        password: password.value
+      firstName: firstName.value,
+      lastName: lastName.value,
+      email: email.value,
+      password: pass,
     };
     console.log(payload);
-    settings = {
-        method: "POST",
-        body: JSON.stringify(payload),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    };
-    console.log(settings);
-    realizarRegister(settings); 
-    // Limpiamos el formulario
-    form.reset()
+    realizarRegister(payload);
+  } else {
+    // Muestra un mensaje de error y limpia los campos de contraseña y contraseña repetida
+    console.warn("Las contraseñas no son iguales.");
+    alert("Las contraseñas no son iguales.");
+    password.value = '';
+    passwordReply.value = '';
+  }
 });
 
 /* ---------------------------------------------- */
 /* FUNCIÓN 2: Realizar el signup [POST] */
 /* ---------------------------------------------- */
-function realizarRegister(settings) {
-};
+function realizarRegister(payload) {
+  if (validarContrasenia(payload.password)) {
+    const settings = {
+      method: "POST",
+      body: JSON.stringify(payload),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+    console.log(settings);
+    // Aquí para enviar los datos al servidor
+  } else {
+    // Muestra un mensaje de error si la contraseña no es segura
+    console.warn("La contraseña no es segura.");
+    alert("La contraseña no es segura.");
+    password.value = '';
+    passwordReply.value = '';
+  }
+}
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,8 +1,11 @@
 /* obtenemos variables globales */
 const url = "https://todo-api.ctd.academy/v1";
 let form = document.forms[0];
+let firstName = document.querySelector("#inputNombre");
+let lastName = document.querySelector("#inputApellido");
 let email = document.querySelector("#inputEmail");
 let password = document.querySelector("#inputPassword");
+let passwordReply = document.querySelector("#inputPasswordRepetida");
 let payload = {}, settings = {};
 
 /* texto */
@@ -19,9 +22,18 @@ function validarEmail(email) {
 function normalizarEmail(email) {
 }
 
-/* assword */
+/* password */
 function validarContrasenia(contrasenia) {
+  let longitud = 8;
+  let contMayus = /[A-Z]/.test(contrasenia);
+  let contMinus = /[a-z]/.test(contrasenia);
+  let contNum = /[0-9]/.test(contrasenia);
+  let contEspec = /[!@#$%^&*()_+{}[\]:;<>,.?~=-]/.test(contrasenia);
+  // Realizar la validación
+  return (contrasenia.length >= longitud && contMayus && contMinus && contNum && contEspec);
 }
 
+
 function compararContrasenias(contrasenia_1, contrasenia_2) {
+  return contrasenia_1 === contrasenia_2;
 }
