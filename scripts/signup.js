34f99b210bef5f8356305053d9d8751a481539fd
JhonatanRios <jhoto1234@gmail.com>
2023-09-07 00:23:28 -0500

    Ajustar signup falta ajustar resetStyle

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,67 +1,21 @@
+let errores = [];
 /* ---------------------------------------------- */
 /* FUNCIÓN 1: Escuchamos el submit y preparamos el envío */
 /* ---------------------------------------------- */
-form.addEventListener('submit', (e) => {
+form.addEventListener('submit', async (e) => {
   e.preventDefault();
-  const pass = password.value;
-  const passRepetida = passwordReply.value;
-
-  let errores = [];
-
-  switch (true) {
-    case !compararContrasenias(pass, passRepetida):
-      agregarError("password", "Las contraseñas no coinciden.");
-      agregarError("passwordReply", "Las contraseñas no coinciden.");
-      password.value = '';
-      passwordReply.value = '';
-      break
-    case !validarTexto(firstName.value):
-      agregarError("firstName", "El nombre no es válido.");
-      firstName.value = '';
-      break
-    case !validarTexto(lastName.value):
-      agregarError("lastName", "El apellido no es válido.");
-      lastName.value = '';
-      break
-    case !validarEmail(email.value):
-      agregarError("email", "El correo electrónico no es válido.");
-      email.value = '';
-      break
-    case compararContrasenias(pass, passRepetida):
-      validarContrasenia(pass);
-      break
-    case !validarContrasenia(pass):
-      agregarError("password", "La contraseña no es segura.");
-      password.value = '';
-      passwordReply.value = ''; 
-      break
-  }
-  console.log(errores);
-
-  function agregarError(campo, mensaje) {
-    errores.push({ campo, mensaje });
-  }
-
-  if (errores.length === 0) {
-    payload = {
-      firstName: normalizarTexto(firstName.value),
-      lastName: normalizarTexto(lastName.value),
-      email: normalizarEmail(email.value),
-      password: pass,
-    };
-    console.log(payload);
-    realizarRegister(payload);
-  } else {
-    errores.forEach((error) => {
-      console.warn(`${error.mensaje}`);
-    });
+  const isValid = validForm();
+  errores = [];
+  if (isValid) {
+    const payload = prepararPayload();
+    await realizarRegister(payload);
   }
 });
 
 /* ---------------------------------------------- */
 /* FUNCIÓN 2: Realizar el signup [POST] */
 /* ---------------------------------------------- */
-function realizarRegister(payload) {
+async function realizarRegister(payload) {
   const settings = {
     method: "POST",
     body: JSON.stringify(payload),
@@ -69,5 +23,91 @@ function realizarRegister(payload) {
       'Content-Type': 'application/json'
     }
   };
-  console.log(settings);
+
+  // try {
+  //   const response = await fetch(`${url}`, settings);
+  //   const data = await response.json();
+  //   console.log(data);
+  // } catch (error) {
+  //   console.error(error);
+  // }
+}
+
+// Función para validar el formulario
+function validForm() {
+  const validFirstName = validarTexto(firstName.value);
+  const validLastName = validarTexto(lastName.value);
+  const isValidEmail = validarEmail(email.value);
+  const validPass = validarContrasenia(password.value);
+  const passEqual = compararContrasenias(password.value, passwordReply.value);
+
+  // Manejar errores y mensajes de error si es necesario
+  if (!passEqual) {
+    addErrorMessage(password, "Las contraseñas no coinciden.");
+    addErrorMessage(passwordReply, "Las contraseñas no coinciden.");
+  }
+  if (!validFirstName) {
+    addErrorMessage(firstName, "El nombre no es válido.");
+  }
+  if (!validLastName) {
+    addErrorMessage(lastName, "El apellido no es válido.");
+  }
+  if (!isValidEmail) {
+    addErrorMessage(email, "El correo electrónico no es válido.");
+  }
+  if (!validPass && passEqual) {
+    addErrorMessage(password, "La contraseña no es segura.");
+    addErrorMessage(passwordReply, "La contraseña no es segura.");
+  }
+  // Devolver true si el formulario es válido, false si hay errores
+  return passEqual && validFirstName && validLastName && isValidEmail && validPass;
+}
+
+// Función para preparar el payload
+function prepararPayload() {
+  // Preparar y retornar el objeto payload
+  const payload = {
+    firstName: normalizarTexto(firstName.value),
+    lastName: normalizarTexto(lastName.value),
+    email: normalizarEmail(email.value),
+    password: password.value,
+  };
+  return payload;
+}
+
+// Función para agregar un mensaje de error
+function addErrorMessage(campo, mensaje) {
+  // Agregar el mensaje de error al arreglo de errores
+  errores.push({ campo, mensaje });
+  // Llamar a la función para mostrar los errores
+  mostrarErrores();
+}
+
+function mostrarErrores() {
+  // Eliminar mensajes de error anteriores
+  form.querySelectorAll(".error-message").forEach((mensajeAnterior) => {
+    resetStyle();
+    mensajeAnterior.remove();
+  });
+  
+  // Recorrer el arreglo de errores y generar mensajes de error
+  errores.forEach((error) => {
+    const { campo, mensaje } = error;
+    const errorSpan = document.createElement("span");
+    errorSpan.textContent = mensaje;
+    errorSpan.classList.add("error-message");
+    errorSpan.style.cssText = "color: red; font-size: 10px; margin: 0 0 10px; text-align: center;";
+    campo.style.cssText += "margin: 0 0 5px;";
+    campo.parentNode.insertBefore(errorSpan, campo.nextSibling);
+  });
+}
+
+function resetStyle() {
+  // Eliminar el estilo margin de los campos sin mensaje de error
+  form.querySelectorAll("input").forEach((input) => {
+    const errorSpan = input.parentNode.querySelector(".error-message");
+    if (!errorSpan) {
+      input.style.margin = ""; // Eliminar el estilo "margin"
+    }
+  });
 }
